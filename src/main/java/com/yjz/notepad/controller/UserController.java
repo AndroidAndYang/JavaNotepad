d8966d1a41028e51f550731d380856394c1d628e
yangjiazhong <925873254@qq.com>
2018-11-26 14:19:54 +0800

    修改user.sql为notepad.sql，并添加了相应的表sql

--- src/main/java/com/yjz/notepad/controller/UserController.java
+++ src/main/java/com/yjz/notepad/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
